b828b5cef25f96be191b62f488a45650f04cb598
ElyriaG <47548802+ElyriaG@users.noreply.github.com>
2021-02-14 20:16:55 -0600

    Server connected

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const colors = require('colors');
-
+const express = require("express");
+const colors = require("colors");
 
 let path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '/public')));
-
-app.use(bodyParser.json());
-
-
-require('./routing/html-routes.js')(app);
-
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, '"index.html"'));
+// });
+app.use(express.static(path.join(__dirname, 'src')))
 
 app.listen(PORT, function () {
-    (console.log(colors.rainbow(`App listening on http://localhost:${PORT}`)))
+  console.log(colors.rainbow(`App listening on http://localhost:${PORT}`));
 });
